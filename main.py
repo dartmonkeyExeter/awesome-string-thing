d61236fc471e8778a17f326a059bb5c3a4bc8b10
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-12-06 10:20:26 +0000

    fiiinaaal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # hello world if it was awesome
 import time
+import os
+
 apostrophe = "'"
 alphabet_and_numbers = list(' ')
 alphabet_and_numbers += [chr(ord('a') + i) for i in range(26)]
@@ -9,9 +11,10 @@ output_string = ""
 final_output = ""
 for idx, i in enumerate(stuff):
     for j in alphabet_and_numbers:
+        os.system('cls')
         output_string = f'{j}'
         print(f'{final_output}{output_string}')
         if j == i:
             break
-        time.sleep(0.02)
+        time.sleep(0.1)
     final_output += output_string
